37d68f8baa60134a177a007134aa140fcd1edfc0
t_seto <t_seto@neton.co.jp>
2020-05-15 11:43:59 +0900

    周波数データではなく音量バーとして表示し、-6db以上の入力を赤く表示

--- src/components/MyCanvas.vue
+++ src/components/MyCanvas.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <canvas id="waveform" width="800" height="400" class="canvas"></canvas>
+        <canvas id="waveform" width="40" height="100" class="canvas"></canvas>
         <p><button  @click="onClick">MyButton</button></p>
     </div>
 </template>
@@ -22,9 +22,6 @@
     export default class MyCanvas extends Vue {
         
         audioCtx: AudioContext = new (window.AudioContext || window.webkitAudioContext)();
-        //voiceSelect = document.getElementById("voice");
-        //source;
-        //stream;
         analyser: AnalyserNode = this.audioCtx.createAnalyser();
         canvas: HTMLCanvasElement | null = null;
         canvasCtx: CanvasRenderingContext2D | null = null;
@@ -35,25 +32,21 @@
           // mounted 以降で canvas の DOM にアクセスできる
           this.getMicStream();
           this.visualize();
-          
         }
-        getMicStream(){
+        async getMicStream(){
             //this.analyser= this.audioCtx.createAnalyser();
             this.analyser.minDecibels = -90;
-            this.analyser.maxDecibels = -10;
-            this.analyser.smoothingTimeConstant = 0.85;
+            this.analyser.maxDecibels = 0;
+            this.analyser.smoothingTimeConstant = 0.9;
             const audioCtx = this.audioCtx;
             const analyser = this.analyser;
-            const distortion = this.audioCtx.createWaveShaper();
             const gainNode = this.audioCtx.createGain();
-            const biquadFilter = this.audioCtx.createBiquadFilter();
-            //const convolver = this.audioCtx.createConvolver();
             const constraints = {audio: true};
-            
-            // if (navigator.mediaDevices === undefined) {
-            //     // プロパティの初期化。readonlyなのでエラーが出る。
-            //     //navigator.mediaDevices = {};
-            // }
+            let stream = null;
+            if (navigator.mediaDevices === undefined) {
+                // プロパティの初期化。readonlyなのでエラーが出る。
+                //navigator.mediaDevices = {};
+            }
             // if (navigator.mediaDevices.getUserMedia === undefined) {
             //     navigator.mediaDevices.getUserMedia = function(constraints) {
                    
@@ -65,41 +58,30 @@
             //         // Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
             //         return new Promise(function(resolve, reject) {
             //             //エラーが出るので一時的にコメントアウト
-            //             getUserMedia.call(navigator, constraints, resolve, reject);
+            //             //getUserMedia.call(navigator, constraints, resolve, reject);
             //         });
             //     }
             // }
             if (navigator.mediaDevices.getUserMedia) {
                 console.log('getUserMedia supported.');
-                navigator.mediaDevices.getUserMedia (constraints)
-                .then(
-                    function(stream) {
-                        const source = audioCtx.createMediaStreamSource(stream);
-                        source.connect(gainNode);
-                        //distortion.connect(biquadFilter);                       
-                        //biquadFilter.connect(gainNode);
-                        //convolver.connect(gainNode);
-                        gainNode.connect(analyser);
-                        analyser.connect(audioCtx.destination);
-                        //this.visualize();
-                })
-                .catch( function(err) { console.log('The following gUM error occured: ' + err);})
-                //こっちで書けるのでは？
-                // try {
-                //   const stream = await navigator.....
-                // } catch(e) {
-                //   console.log(e)
-                // }
+                try {
+                  stream = await navigator.mediaDevices.getUserMedia (constraints)
+                  const source = audioCtx.createMediaStreamSource(stream);
+                  source.connect(gainNode);
+                  gainNode.connect(analyser);
+                  analyser.connect(audioCtx.destination);
+                } catch(e) {
+                  console.log(e)
+                }
             } else {
                 console.log('getUserMedia not supported on your browser!');
             }
         }
         async onClick(){
             window.cancelAnimationFrame(this.frameCount);
-            //this.getMicStream();
-            this.visualize();
+            this.visualize();           
         }
-
+        //周波数ごとのデータから取得
         visualize() {
             
             this.canvas = document.getElementById('waveform') as HTMLCanvasElement;
@@ -114,28 +96,65 @@
             this.frameCount = requestAnimationFrame(this.visualize);
             this.analyser.fftSize = 32;
             const bufferLengthAlt = this.analyser.frequencyBinCount;
-            console.log(bufferLengthAlt);
             const dataArrayAlt = new Uint8Array(bufferLengthAlt);
 
-            this.canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
-
+            this.canvasCtx.fillStyle = 'rgb(0, 0, 0)';
+            this.canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+            if (!(this.canvasCtx instanceof CanvasRenderingContext2D)) {
+                throw new Error("#canvasCtx is not an CanvasRenderingContext2D");
+            }
             this.analyser.getByteFrequencyData(dataArrayAlt);
+            const lowest = 0.01;
+            let max = lowest;
+            for (const f of dataArrayAlt) {
+                max = Math.max(max, (f - 128) / 128);
+            }
+            const normalized = (Math.log(lowest) - Math.log(max)) / Math.log(lowest);
+            const percent = Math.min(Math.max(normalized, 0), 1);
+                          
+    
+            this.canvasCtx.fillStyle ='rgb(0,100,0)';
+                this.canvasCtx.fillRect(0,HEIGHT - HEIGHT * percent, WIDTH, HEIGHT * percent);
+            const peak = 0.5; 
+            if(percent > peak){
+                this.canvasCtx.fillStyle ='rgb(100,0,0)';
+                this.canvasCtx.fillRect(0,HEIGHT - HEIGHT * percent, WIDTH, HEIGHT * (percent - peak)); 
+            }
+            console.log(normalized);  
+        }
+        
+        //波形から取得
+        waveform() {
+            this.canvas = document.getElementById('waveform') as HTMLCanvasElement;
+            this.canvasCtx = this.canvas.getContext('2d');
+            const WIDTH = this.canvas.width;
+            const HEIGHT = this.canvas.height;
+           
+
+            if (!(this.canvasCtx instanceof CanvasRenderingContext2D)) {
+                throw new Error("#canvasCtx is not an CanvasRenderingContext2D");
+            }
+            this.frameCount = requestAnimationFrame(this.waveform);
+            this.analyser.fftSize = 32;
+            const bufferLength = this.analyser.fftSize;
+
+            const dataArray = new Uint8Array(bufferLength);
+
+            //this.canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+
+            this.analyser.getByteTimeDomainData(dataArray);
 
             this.canvasCtx.fillStyle = 'rgb(0, 0, 0)';
             this.canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
             
-            const barWidth = 60;
-            let barHeight;
-            let x = 0;
+            const barWidth = WIDTH;
             this.canvasCtx.fillStyle = 'rgb(100, 0, 0)';
-            for(let i = 0; i < bufferLengthAlt; i++) {
-                barHeight = dataArrayAlt[i];
-
-                this.canvasCtx.fillStyle = 'rgb(' + (barHeight+100) + ',50,50)';
-                this.canvasCtx.fillRect(x,HEIGHT - barHeight/2,barWidth, barHeight/2);
-
-                x += barWidth + 1;
-            }      
+            //dataArray.map(x => Math.abs(x - 128));
+            //const barHeight = dataArray.reduce((acc, item) => acc + item, 0) / bufferLength;
+            let barHeight = dataArray[0];
+            barHeight = Math.abs(barHeight - 128);
+            this.canvasCtx.fillRect(0,HEIGHT - barHeight,barWidth, barHeight);
+            //console.log(barHeight);
         }
     }
 </script>
